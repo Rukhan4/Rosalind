0a63c686bac779a9758ebaf5ad35fc89c1dd28db
Surur Khan <surur.rkhan@gmail.com>
2025-03-01 20:09:26 -0500

    cmd line usage update

--- Transcribing DNA into RNA/solution.py
+++ Transcribing DNA into RNA/solution.py
@@ -1,16 +1,27 @@
+import sys
+
 def transcribe_dna_to_rna(dna):
     return dna.replace('T', 'U')
 
 
 if __name__ == "__main__":
-    with open('./data/rosalind_rna.txt', 'r') as f:
-        dna = f.read().strip()
+    if len(sys.argv) < 3:
+        print("Usage: python solution.py <input_file> <output_file>")
+        sys.exit(1)
 
-    rna = transcribe_dna_to_rna(dna)
-    print(rna)
-    with open('./data/output.txt', 'w') as f:
-        f.write(rna)
+    input_file = sys.argv[1]  # Get input file from command-line argument
+    output_file = sys.argv[2]  # Get output file from command-line argument
 
-    # Output to data/output.txt
-    # python3 solution.py > ./data/output.txt
-    # cat ./data/output.txt
+    try:
+        with open(input_file, 'r') as data:
+            dna = data.read().strip()
+
+        result = transcribe_dna_to_rna(dna)
+
+        with open(output_file, 'w') as output_data:
+            output_data.write(result)
+
+        print(result)  # Print to console for immediate feedback
+    except FileNotFoundError:
+        print(f"Error: File '{input_file}' not found.")
+        sys.exit(1)

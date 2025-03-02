804e7c506123deb065a6a20b73a9268fa55f77c4
Surur Khan <surur.rkhan@gmail.com>
2025-03-01 19:35:15 -0500

    Solved

--- Complementing a Strand of DNA/solution.py
+++ Complementing a Strand of DNA/solution.py
@@ -1,7 +1,17 @@
+def reverse_complement(dna):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    complemented_dna = ''.join([complement[base] for base in dna])
+    reverse_complement = complemented_dna[::-1]
+    return reverse_complement
 
 
-
-
+if __name__ == "__main__":
+    with open('data/rosalind_revc.txt', 'r') as data:
+        dna = data.read().strip()
+        print(reverse_complement(dna))
+    
+    with open('data/output.txt', 'w') as output_data:
+        output_data.write(reverse_complement(dna))
 
 
 
